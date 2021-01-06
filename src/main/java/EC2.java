481c618d2c8fea1e9097e96de0ea30667ab59ffe
Ziwen Wang <zi.w.wang@mail.mcgill.ca>
2021-01-06 00:29:23 -0500

    adding getAllInstance method adding filterInstanceByStatus method modify createInstance to createDefaultInstance

--- src/main/java/EC2.java
+++ src/main/java/EC2.java
@@ -1,4 +1,5 @@
-
+import java.util.List;
+import java.util.ArrayList;
 import com.amazonaws.services.ec2.model.*;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
@@ -6,10 +7,12 @@ import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 
 
 interface EC2_Interface{
-    public RunInstancesResult createInstance();
-    public void describeAllInstances();
-    public TerminateInstancesResult deleteInstance(String instanceID);
-    public RebootInstancesResult rebootInstance(String instanceID);
+    RunInstancesResult createDefaultInstance();
+    void describeAllInstances();
+    List<Instance> getAllInstances();
+    TerminateInstancesResult deleteInstance(String instanceID);
+    RebootInstancesResult rebootInstance(String instanceID);
+    List<Instance> filterInstanceByStatus(InstanceStateName state);
 }
 
 public class EC2 implements EC2_Interface {
@@ -25,7 +28,9 @@ public class EC2 implements EC2_Interface {
     }
 
     @Override
-    public RunInstancesResult createInstance() {
+    public RunInstancesResult createDefaultInstance() {
+         // create default instance with pre-defined AMIs, Instance Type, Name of Key Pair, Security Group ID and SubnetID
+        // return the RunInstancesResult and also print out if the creation is success or not
          RunInstancesRequest runInstancesRequest = new RunInstancesRequest()
                 .withImageId(imageID)
                 .withInstanceType(InstanceType.T2Micro)
@@ -36,11 +41,39 @@ public class EC2 implements EC2_Interface {
                 .withSubnetId(subnetID);
 
         RunInstancesResult result = ec2.runInstances(runInstancesRequest);
+        if (result.getReservation().getInstances().get(0).getInstanceId() != null){
+            System.out.println("EC2 instance with ID " + result.getReservation().getInstances().get(0).getInstanceId() + "has been successfully created.");
+        }
         return result;
+
     }
 
     @Override
     public void describeAllInstances() {
+        // Print out the detail information about all EC2 instances
+        List<Instance> instances = this.getAllInstances();
+
+        for(Instance instance : instances) {
+            System.out.printf(
+                    "Found instance with id %s, " +
+                            "AMI %s, " +
+                            "type %s, " +
+                            "state %s " +
+                            "and monitoring state %s",
+                    instance.getInstanceId(),
+                    instance.getImageId(),
+                    instance.getInstanceType(),
+                    instance.getState().getName(),
+                    instance.getMonitoring().getState());
+            System.out.println();
+        }
+
+    }
+
+    @Override
+    public List<Instance> getAllInstances() {
+        // get list of Instance object of all EC2 instances and return the list
+        List<Instance> instances = new ArrayList<>();
         boolean done = false;
 
         DescribeInstancesRequest request = new DescribeInstancesRequest();
@@ -49,19 +82,8 @@ public class EC2 implements EC2_Interface {
 
             for(Reservation reservation : response.getReservations()) {
                 for(Instance instance : reservation.getInstances()) {
-                    System.out.printf(
-                            "Found instance with id %s, " +
-                                    "AMI %s, " +
-                                    "type %s, " +
-                                    "state %s " +
-                                    "and monitoring state %s",
-                            instance.getInstanceId(),
-                            instance.getImageId(),
-                            instance.getInstanceType(),
-                            instance.getState().getName(),
-                            instance.getMonitoring().getState());
+                    instances.add(instance);
                 }
-                System.out.println();
             }
 
             request.setNextToken(response.getNextToken());
@@ -70,10 +92,12 @@ public class EC2 implements EC2_Interface {
                 done = true;
             }
         }
+        return instances;
     }
 
     @Override
     public TerminateInstancesResult deleteInstance(String instanceID) {
+        // with given instance ID, terminate this instance
         TerminateInstancesRequest deleteRequest = new TerminateInstancesRequest()
                 .withInstanceIds(instanceID);
         TerminateInstancesResult deleteResponse = ec2.terminateInstances(deleteRequest);
@@ -83,7 +107,28 @@ public class EC2 implements EC2_Interface {
 
     @Override
     public RebootInstancesResult rebootInstance(String instanceID) {
-        return null;
+        //reboot the instance by using given instance ID
+        RebootInstancesRequest request = new RebootInstancesRequest()
+                .withInstanceIds(instanceID);
+
+        RebootInstancesResult response = ec2.rebootInstances(request);
+
+        System.out.printf(
+                "Successfully rebooted instance %s", instanceID);
+        return response;
+    }
+
+    @Override
+    public List<Instance> filterInstanceByStatus(InstanceStateName state) {
+        // filter all instances by given state
+        List<Instance> instances = this.getAllInstances();
+        List<Instance> filteredResult = new ArrayList<>();
+        for(Instance instance : instances) {
+            if(instance.getState().getName().equals(state.toString())){
+                filteredResult.add(instance);
+            }
+        }
+        return filteredResult;
     }
 
 
@@ -95,10 +140,15 @@ class Demo{
         EC2 ec2 = new EC2();
         ec2.describeAllInstances();
 
-        //RunInstancesResult result = ec2.createInstance();
-        //System.out.println(result);
+//        RunInstancesResult result = ec2.createDefaultInstance();
+//        System.out.println(result);
 
-        //TerminateInstancesResult result = ec2.deleteInstance("i-03e3db40c82ae5bf7");
-        //System.out.println(result);
+        List<Instance> filteredResult = ec2.filterInstanceByStatus(InstanceStateName.Running);
+        System.out.println(filteredResult);
+
+//        RebootInstancesResult rebootResult = ec2.rebootInstance(filteredResult.get(0).getInstanceId());
+//        System.out.println(rebootResult);
+//        TerminateInstancesResult result = ec2.deleteInstance(filteredResult.get(0).getInstanceId());
+//        System.out.println(result);
     }
 }
